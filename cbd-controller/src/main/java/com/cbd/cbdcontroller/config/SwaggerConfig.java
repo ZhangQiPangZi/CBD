6661f01b9dc645ec11832495fae99a8e3e7d2e10
e <1131920009@qq.com>
2020-05-09 21:23:23 +0800

    添加跨域

--- cbd-controller/src/main/java/com/cbd/cbdcontroller/config/SwaggerConfig.java
+++ cbd-controller/src/main/java/com/cbd/cbdcontroller/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.cbd.cbdcontroller.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -43,8 +44,19 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
         // 解决swagger的js文件无法访问
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
-        super.addResourceHandlers(registry);
     }
 
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //设置允许跨域的路径
+        registry.addMapping("/**")
+                //设置允许跨域请求的域名
+                .allowedOrigins("*")
+                //是否允许证书 不再默认开启
+                .allowCredentials(true)
+                //设置允许的方法
+                .allowedMethods("*")
+                //跨域允许时间
+                .maxAge(3600);
+    }
 }
